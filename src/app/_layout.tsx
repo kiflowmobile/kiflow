5c093add5068fba4a84a91be4c308e083a123b1d
natamrshn <natamrshn@gmail.com>
2025-10-01 16:00:06 +0100

    fix home screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,14 @@
-import "react-native-reanimated"; // ⚡️ цей імпорт має бути найпершим
-
+import 'react-native-reanimated'; // ⚡️ цей імпорт має бути найпершим
 
 import { useAuthStore } from '@/src/stores/authStore';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   const checkSession = useAuthStore((state) => state.checkSession);
@@ -26,20 +24,18 @@ export default function RootLayout() {
   }
 
   return (
-
     <SafeAreaProvider>
-  <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="course-code" />
-    <Stack.Screen name="home" />
-    
-    <Stack.Screen name="courses/index" />
-    <Stack.Screen name="courses/[id]" />
-    <Stack.Screen name="instractions" />
-    <Stack.Screen name="auth/login" />
-    <Stack.Screen name="auth/registration" />
-    <Stack.Screen name="+not-found" />
-  </Stack>
-</SafeAreaProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="course-code" />
+        <Stack.Screen name="home" />
+        <Stack.Screen name="courses/index" />
+        <Stack.Screen name="courses/[id]" />
+        <Stack.Screen name="instractions" />
+        <Stack.Screen name="auth/login" />
+        <Stack.Screen name="auth/registration" />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
