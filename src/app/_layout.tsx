3f6195156e0d21269cfe8ff9f88e51828b1b3147
natamrshn <natamrshn@gmail.com>
2025-10-08 12:19:45 +0100

    add header

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,19 @@
-import "react-native-reanimated"; // ⚡️ цей імпорт має бути найпершим
-
+import 'react-native-reanimated'; // ⚡️ цей імпорт має бути найпершим
 
 import { useAuthStore } from '@/src/stores/authStore';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { useEffect } from 'react';
-import { Platform } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useUserProgressStore } from "../stores";
-import { initUserProgress } from "../services/course_summaries";
+import { useUserProgressStore } from '../stores';
+import CustomHeader from '../components/ui/CustomHeader';
 
 export default function RootLayout() {
   const { initFromLocal } = useUserProgressStore();
-  const {  user } = useAuthStore();
+  const { user } = useAuthStore();
 
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
   const checkSession = useAuthStore((state) => state.checkSession);
@@ -25,29 +23,32 @@ export default function RootLayout() {
   }, [checkSession]);
 
   useEffect(() => {
-    initFromLocal(); 
+    initFromLocal();
   }, [user]);
 
   if (!loaded) {
     return null;
   }
 
-
   return (
-
     <SafeAreaProvider>
-  <Stack screenOptions={{ headerShown: false }}>
-    <Stack.Screen name="index" />
-    <Stack.Screen name="course-code" />
-    <Stack.Screen name="home" />
-    <Stack.Screen name="module/[moduleId]" /> 
-    <Stack.Screen name="courses/index" />
-    <Stack.Screen name="courses/[id]" />
-    <Stack.Screen name="instractions" />
-    <Stack.Screen name="auth/login" />
-    <Stack.Screen name="auth/registration" />
-    <Stack.Screen name="+not-found" />
-  </Stack>
-</SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          header: () => <CustomHeader />,
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="course-code" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
+        <Stack.Screen name="module/[moduleId]" options={{ headerShown: false }} />
+        <Stack.Screen name="auth/login" options={{ headerShown: false }} />
+        <Stack.Screen name="auth/registration" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" options={{ headerShown: false }} />
+        <Stack.Screen name="courses/index" />
+        <Stack.Screen name="courses/[id]" />
+        <Stack.Screen name="instractions" />
+        <Stack.Screen name="profile" />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
