7980196b3afb6726548104136041b519081bfc4c
Platon Rov <platon.rov@gmail.com>
2026-01-27 21:00:50 +0100

    fix: eye icon on input

---
+++ components/icons/eye-closed-icon.tsx
@@ -0,0 +1,34 @@
+export const EyeClosedIcon = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.5 15.9997L17.0248 12.6035"
+        stroke={props.stroke || '#D4D4D4'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 17.5V14"
+        stroke={props.stroke || '#D4D4D4'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.5 16.0009L6.96895 12.6133"
+        stroke={props.stroke || '#D4D4D4'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M3 8C6.6 16 17.4 16 21 8"
+        stroke={props.stroke || '#D4D4D4'}
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
